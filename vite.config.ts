2da56dfb22c6261a14c28c368d484aad2fc7d19b
Jan Forst <ensonador@gmail.com>
2026-04-26 11:05:16 +0200

    pwa + svgo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite-plus'
+import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'MIDI of Hearts',
+        short_name: 'MoH',
+        description: 'Bluetooth smartwatch heart rate monitor and MIDI clock',
+        background_color: '#26262a',
+        theme_color: '#bb3b37',
+        lang: 'en',
+      },
+    }),
+    viteSingleFile(),
+  ],
   base: './',
+  build: {
+    cssCodeSplit: false, // optional: combine all CSS into one
+    assetsInlineLimit: 1000000, // optional: inline small assets too
+  },
   fmt: {
     semi: false,
     singleQuote: true,
